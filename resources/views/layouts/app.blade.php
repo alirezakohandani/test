a7b1daf3cdd1a4483ba85d778267138d5c3d6dac
alirezakohandani <alireza.ko73@gmail.com>
2021-01-12 18:07:04 +0330

    improve UI(navbar)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,23 +2,9 @@
  
 <body>
     <section id="basic-buttons">
-        <div class="row">
-            <div class="col-12">
-              <div class="card">
-                <div class="card-header">
-                </div>
-                <div class="card-content">
-                  <div class="card-body">
-                    <div class="row">
-                      <div class="col-12">
+      
     @include('partials.navbar')
-</div>
-</div>
-</div>
-</div>
-</div>
-</div>
-</div>
+
 </section>
     </section>
     <section id="basic-vertical-layouts">
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,21 +1,63 @@
 
-   
-               
-<a href="{{ route('shop') }}">فروشگاه</a>   
-<br>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+            <nav class="navbar navbar-expand-lg navbar-light bg-default">
+                
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                  <ul class="navbar-nav">
+                    <li class="nav-item active">
+                      <a class="nav-link" href="{{ route('home') }}">صفحه اصلی <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="{{ route('shop') }}">فروشگاه</a>
+                    </li>
+                      <li class="nav-item dropdown">
+                      <a href="{{ route('cart.show') }}"><div class="fonticon-wrap"><i class="fa fa-shopping-cart fa-2x"></i></div></a>
+                      </li>
+                  </ul>
 
-       
-@guest
-<a href="{{ route('auth.register.form') }}"><button type="button" class="btn btn-success mr-1 mb-1 waves-effect waves-light">ثبت نام</button></a>
-<a href="{{ route('auth.login.form') }}"><button type="button" class="btn btn-info mr-1 mb-1 waves-effect waves-light">ورود</button></a>
-@endguest
+            
+                </div>
+                @auth
+              
+                  <form class="form-inline my-2 my-lg-0">
+                    @if (Auth::user()->role == 1)
+                    <a href="{{ route('admin.dashboard') }}"><button type="button" class="btn btn-info mr-1 mb-1 waves-effect waves-light">پنل</button></a>
+                       @endif
+                  </form>
+                  @endauth
+     
+                @guest
+                <form class="form-inline my-2 my-lg-0">
+                    <a href="{{ route('auth.register.form') }}"><button type="button" class="btn btn-success mr-1 mb-1 waves-effect waves-light">ثبت نام</button></a>
+                    <a href="{{ route('auth.login.form') }}"><button type="button" class="btn btn-info mr-1 mb-1 waves-effect waves-light">ورود</button></a>
+                  </form>
+                @endguest
+                @auth
+                <form class="form-inline my-2 my-lg-0">
 
-@auth
-   <p>سلام {{ Auth::user()->name }}</p>
-   <a href="{{ route('auth.logout') }}"><button type="button" class="btn btn-danger mr-1 mb-1 waves-effect waves-light">خروج</button></a>
-   @if (Auth::user()->role == 1)
+                    <a href="{{ route('auth.logout') }}"><button type="button" class="btn btn-danger mr-1 mb-1 waves-effect waves-light">خروج</button></a>
+                </form>
+                @endauth
+              </nav>
+            </div>
+        </div>
+    </div>
+
+  
+       <br>
+       <br>
+       @auth
+       <p class="text-left">سلام {{ Auth::user()->name }}</p>
+       @if (Auth::user()->role == 1)
        نقش : ادمین
-       <a href="{{ route('admin.dashboard') }}"><button type="button" class="btn btn-info mr-1 mb-1 waves-effect waves-light">پنل</button></a>
+       </html>
    @endif
-@endauth
-<a href="{{ route('cart.show') }}"><div class="fonticon-wrap"><i class="fa fa-shopping-cart fa-2x"></i></div></a>
+       @endauth
+  
+
+
