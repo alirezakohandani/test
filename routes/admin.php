6649a3b5cd5f9cf50219945d8e0751b856dd0115
alirezakohandani <alireza.ko73@gmail.com>
2021-01-29 02:10:36 +0330

    create file update process

--- routes/admin.php
+++ routes/admin.php
@@ -22,7 +22,7 @@ Route::group(['prefix' => 'admin/', 'namespace' => 'Admin', 'as' => 'admin.'], f
     Route::post('file/upload', 'FileController@store')->name('file.send');
     Route::post('file/upload/ajax', 'FileController@storeWithAjax')->name('file.send.ajax');
     Route::get('file/update/{id}', 'FileController@showUpdateForm')->name('file.form.update');
-    Route::post('file/update', 'FileController@update')->name('file.update');
+    Route::post('file/update/', 'FileController@update')->name('file.update');
     Route::get('manage', 'FileController@showMange')->name('file.manage');
     Route::delete('manage/delete/{id}', 'FileController@delete')->name('file.delete');
     Route::get('post/upload', 'PostController@showPostFrom')->name('post.form');
